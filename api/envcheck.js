76a720df507a452dd97cbb377b9664c97dd82a18
Isac <isac@slotessentials.com>
2026-07-22 14:59:08 -0400

    remove temp envcheck

--- api/envcheck.js
+++
@@ -1,6 +0,0 @@
-// TEMPORARY debug: lists env var NAMES (never values) related to storage. Remove after setup.
-module.exports = (req, res) => {
-  const names = Object.keys(process.env).filter((k) => /KV|REDIS|UPSTASH|STORAGE/i.test(k)).sort();
-  res.setHeader("Cache-Control", "no-store");
-  res.status(200).json({ names });
-};
